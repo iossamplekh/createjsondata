f5853670c213b8016a9acf74278d74f462dd996a
rithlkh2 <rithronlkh@gmail.com>
2018-02-17 11:26:56 +0700

    add refector

--- Contents.swift
+++ Contents.swift
@@ -3,16 +3,15 @@
 import UIKit
 
 let jsonDict = [
-    "name" : "Swift",
-    "developer" : "Apple Inc.",
-    "releaseDate" : [
-        "format":"longStyle",
-        "date": "June 2, 2014"
-    ],
-    "typing": ["static", "strong", "inferred"],
-    "OS": ["Mac OS", "Linux", "FreeBSD"],
-    "license": "Apache License 2.0",
-    "website": "swift.org"
+    "newsObj" : [
+                "name":"nameValue"
+                ],
+    "newsTypeObj" : [
+                "name":"nameValue"
+                ],
+    "authorObj" : [
+                "name":"nameValue"
+            ]
     ] as [String : Any]
 if JSONSerialization.isValidJSONObject(jsonDict) {
     if let data = try? JSONSerialization.data(withJSONObject: jsonDict, options: []) {
