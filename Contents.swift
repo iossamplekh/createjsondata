6f889f107e45510380d0096cfe1e1cb6dab2853b
rithlkh2 <rithronlkh@gmail.com>
2018-02-17 13:11:19 +0700

    add custome

--- Contents.swift
+++ Contents.swift
@@ -1,18 +1,38 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+class NewsHelper{
+    var name: String?
+    var newsTypeDesEn: String?
+    var desEn: String?
+}
+
+var newsObject = NewsHelper()
+newsObject.name = "NnameValue"
+newsObject.newsTypeDesEn = "NnewsTypeValue"
+newsObject.desEn = "NdesEnValue"
+
+var newsTypeObjectArr = [String]()
+newsTypeObjectArr.append("AdesEnValue")
+newsTypeObjectArr.append("AdesEnValue2")
+newsTypeObjectArr.append("AdesEnValue3")
+
+var authorObjectArr = [String]()
+authorObjectArr.append("nameValue")
+authorObjectArr.append("nameValue2")
+authorObjectArr.append("nameValue3")
 
 let jsonDict = [
     "newsObj" : [
-                "name":"nameValue",
-                "newsTypeDesEn":"newsTypeValue",
-                "desEn":"desEnValue"
+                "name":newsObject.name!,
+                "newsTypeDesEn":newsObject.newsTypeDesEn!,
+                "desEn":newsObject.desEn!
                 ],
-    "newsTypeObj" :[
-                "desEn":["desEnValue","desEnValue2","desEnValue3"]
+    "newsTypeObj" : [
+                "desEn":newsTypeObjectArr
                 ],
     "authorObj" : [
-                "name":["nameValue","nameValue2","nameValue3"]
+                "name":authorObjectArr
             ]
     ] as [String : Any]
 if JSONSerialization.isValidJSONObject(jsonDict) {
